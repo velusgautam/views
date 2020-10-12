64db36a1c4026bd8a181998646d80cea024d6810
Velu Gautam <velu.gautam@comeon.com>
2020-10-13 01:21:35 +0530

    Only github or views.hectane.com

--- router.js
+++ router.js
@@ -5,44 +5,62 @@ const Views = require('./views/views.model');
 const fs = require('fs');
 
 router.get('/*.svg', async (req, res) => {
-  // console.log(new URL(req.url));
   console.log('host', req.headers);
-  //   console.log(counter);
-  const url = path.join(req.headers.host, req.path);
-
-  console.log(url);
-  //   if (counter[url]) {
-  //     counter[url] = counter[url] + 1;
-  //   } else {
-  //     counter[url] = 1;
-  //   }
-
-  try {
-    const views = await Views.findOneAndUpdate(
-      { url: url },
-      { $inc: { count: 1 } },
-      { upsert: true, new: true, fields: { count: true, _id: false } }
-    );
-    // const count = await Hits.countDocuments();
-    // console.log('Count', count);
-    console.log(views);
+  if (
+    (req.headers.via &&
+      req.headers.via.includes('github-camo') &&
+      req.headers['user-agent'] &&
+      req.headers['user-agent'].includes('github-camo')) ||
+    (req.headers['sec-fetch-site'] &&
+      req.headers['sec-fetch-site'] === 'same-origin' &&
+      req.headers.referer &&
+      req.headers.referer === 'https://views.hectane.com/')
+  ) {
+    const url = path.join(req.headers.host, req.path);
+    console.log(url);
+    try {
+      const views = await Views.findOneAndUpdate(
+        { url: url },
+        { $inc: { count: 1 } },
+        { upsert: true, new: true, fields: { count: true, _id: false } }
+      );
+      // const count = await Hits.countDocuments();
+      // console.log('Count', count);
+      console.log(views);
+      res.contentType('image/svg+xml')
+        .send(`<svg xmlns="http://www.w3.org/2000/svg" width="90" height="20">
+    <rect width="40" height="20" fill="#555" />
+    <rect x="40" width="50" height="20" fill="#4c1" />
+    <rect rx="3" width="90" height="20" fill="transparent" />
+    <g
+      fill="#fff"
+      text-anchor="middle"
+      font-family="Verdana,Geneva,sans-serif"
+      font-size="10"
+    >
+      <text x="18" y="14">Views</text>
+      <text x="64" y="14">${views.count}</text>
+    </g>
+  </svg>`);
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  } else {
     res.contentType('image/svg+xml')
       .send(`<svg xmlns="http://www.w3.org/2000/svg" width="90" height="20">
-  <rect width="40" height="20" fill="#555" />
-  <rect x="40" width="50" height="20" fill="#4c1" />
-  <rect rx="3" width="90" height="20" fill="transparent" />
-  <g
-    fill="#fff"
-    text-anchor="middle"
-    font-family="Verdana,Geneva,sans-serif"
-    font-size="10"
-  >
-    <text x="18" y="14">Views</text>
-    <text x="64" y="14">${views.count}</text>
-  </g>
-</svg>`);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
+    <rect width="40" height="20" fill="#555" />
+    <rect x="40" width="50" height="20" fill="#4c1" />
+    <rect rx="3" width="90" height="20" fill="transparent" />
+    <g
+      fill="#fff"
+      text-anchor="middle"
+      font-family="Verdana,Geneva,sans-serif"
+      font-size="10"
+    >
+      <text x="18" y="14">Views</text>
+      <text x="64" y="14">1</text>
+    </g>
+  </svg>`);
   }
 });
 if (process.env.IS_LOCAL === 'true') {
